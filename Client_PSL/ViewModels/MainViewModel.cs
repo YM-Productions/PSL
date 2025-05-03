630eeafc261b06ff59a7859775525157c8c5d045
YaeneZ <yanic.zehnder@gmail.com>
2025-05-03 12:55:41 +0200

    Binding Generation WIndows --- - Added automatic Binding Generation on Build for Windows

--- Client_PSL/ViewModels/MainViewModel.cs
+++ Client_PSL/ViewModels/MainViewModel.cs
@@ -1,18 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-using SpacetimeDB;
-using SpacetimeDB.Types;
-using System.Collections.Concurrent;
-
 namespace Client_PSL.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _greeting = "Welcome to Avalonia!";
-
-    Identity? local_identity = null;
-    ConcurrentQueue<(string Command, string Args)> input_queue = new();
 
     public MainViewModel()
     {
--- Client_PSL/prebuild.bat
+++ Client_PSL/prebuild.bat
@@ -1,19 +1,19 @@
 @echo off
+REM === Set absolute path to script directory ===
+set SCRIPT_DIR=%~dp0
+set LOG_FILE=%SCRIPT_DIR%preBuildOutput.txt
 
-REM Log-File
-set LOG_FILE=preBuildOutput.txt
+REM === Start Logging ===
+echo Starting... > "%LOG_FILE%"
+echo Script started at %DATE% %TIME% >> "%LOG_FILE%"
 
-REM Starting Logging
-echo Starting... > %LOG_FILE%
-echo Batch-Script startet at %DATE% %TIME% >> %LOG_FILE%
+REM === Change to parent directory ===
+cd /d %SCRIPT_DIR%\..
+echo Switched to directory: %CD% >> "%LOG_FILE%"
 
-REM change to Parent Dir
-cd /d %~dp0
-echo Switched directory /../ >> %LOG_FILE%
+REM === Generate SpacetimeDB bindings ===
+echo Generating SpacetimeDB Bindings at %DATE% %TIME% >> "%LOG_FILE%"
+spacetime generate --lang csharp --out-dir Client_PSL/module_bindings --project-path server >> "%LOG_FILE%" 2>&1
+echo Generation finished at %DATE% %TIME% >> "%LOG_FILE%"
 
-echo Starting to Generate SpacetimeDB Bindings at %DATE% %TIME% >> %LOG_FILE%
-REM Generate SpacetimeDB bindings for Client_PSL
-spacetime generate --lang csharp --out-dir Client_PSL/module_bindings --project-path server >> %LOG_FILE% 2>&1
-echo Generating SpacetimeDB Bindings ended at %DATE% %TIME% >> %LOG_FILE%
-
-echo Batch-Script ended at %DATE% %TIME% >> %LOG_FILE%
+echo Script ended at %DATE% %TIME% >> "%LOG_FILE%"
