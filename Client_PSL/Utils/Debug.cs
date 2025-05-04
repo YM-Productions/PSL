06ebab520c31807a29142cec9d5d12a9549463fe
YaeneZ <yanic.zehnder@gmail.com>
2025-05-04 13:45:02 +0200

    Added some test Doc --- - Added Doc for Debug.Log

--- Client_PSL/Utils/Debug.cs
+++ Client_PSL/Utils/Debug.cs
@@ -4,6 +4,16 @@ namespace Utils;
 
 public static class Debug
 {
+    /// <summary>
+    /// This Method Logs always on Lvl 5 as INFO Logger
+    /// <example>
+    /// <code>
+    /// // Prints Hello to Logs
+    /// Debug.Log("Hello");
+    /// </code>
+    /// </example>
+    /// </summary>
+    /// <param name="message">The Message to Log</param>
     public static void Log(string message) => Logger.LoggerFactory.GetLogger("INFO").Log(message);
     public static void LogWarning(string message) => Logger.LoggerFactory.GetLogger("WARNING").Log(message);
     public static void LogError(string message) => Logger.LoggerFactory.GetLogger("ERROR").Log(message);
