94eab5e2f60b0adb44e13e93e34da570f3c2ac40
Yaene <yanic.zehnder@gmail.com>
2025-05-30 13:48:12 +0200

    ClientLogs
    - Added Logs for Client in all Reducers of PhysicalObjectSystem.cs

--- server/Modules/POS/PhysicalObjectSystem.cs
+++ server/Modules/POS/PhysicalObjectSystem.cs
@@ -180,6 +180,8 @@ public static partial class Module
 
             ctx.Db.PhysicalObject.Insert(foundation);
             ctx.Db.PhysicalObjectPermission.Insert(permission);
+
+            ClientLog.Info(ctx, $"Created foundation {foundation.Name} at ({foundation.XPos}, {foundation.YPos}) in plot {plotID}.");
         }
 
         [Reducer]
@@ -199,6 +201,8 @@ public static partial class Module
 
             ctx.Db.PhysicalObject.Insert(vehicle);
             ctx.Db.PhysicalObjectPermission.Insert(permission);
+
+            ClientLog.Info(ctx, $"Created vehicle {vehicle.Name} at ({vehicle.XPos}, {vehicle.YPos}) in plot {plotID}.");
         }
 
         [Reducer]
@@ -206,24 +210,25 @@ public static partial class Module
         {
             if (ctx.Sender.Equals(targetIdentity))
             {
-                // Prevent users from changing their own permissions
+                ClientLog.Warning(ctx, "Cannot set permission for yourself.");
                 return;
             }
 
             string actingPermID = PhysicalObjectPermission.GetIdentity(ctx.Sender, physicalObjectID);
             string targetPermID = PhysicalObjectPermission.GetIdentity(targetIdentity, physicalObjectID);
 
-            if (ctx.Db.PhysicalObjectPermission.identity.Find(actingPermID) is not PhysicalObjectPermission actingPerm)
+            if (ctx.Db.PhysicalObjectPermission.identity.Find(actingPermID) is not PhysicalObjectPermission actingPerm ||
+                actingPerm.Level < Permission.Level.Admin)
+            {
+                ClientLog.Warning(ctx, "You do not have permission to set permissions for this physical object.");
                 return;
-
-            if (actingPerm.Level < Permission.Level.Admin)
-                return;
+            }
 
             if (ctx.Db.PhysicalObjectPermission.identity.Find(targetPermID) is PhysicalObjectPermission targetPerm)
             {
                 if (targetPerm.Level >= actingPerm.Level)
                 {
-                    // Cannot lower permission of a higher/equal level permission
+                    ClientLog.Warning(ctx, "Cannot lower permission of a higher or equal level permission.");
                     return;
                 }
 
@@ -235,7 +240,7 @@ public static partial class Module
                 {
                     if (actingPerm.Level < level)
                     {
-                        // Cannot set permission to something higher than your own
+                        ClientLog.Warning(ctx, "Cannot set permission to something higher than your own.");
                         return;
                     }
                     targetPerm.Level = level;
@@ -245,80 +250,137 @@ public static partial class Module
             else
             {
                 if (level == Permission.Level.None)
+                {
+                    ClientLog.Warning(ctx, "Cannot set permission to None for a user that does not have permission.");
                     return;
+                }
 
                 if (actingPerm.Level < level)
                 {
-                    // Cannot set permission to something higher than your own
+                    ClientLog.Warning(ctx, "Cannot set permission to something higher than your own.");
                     return;
                 }
                 PhysicalObjectPermission newPoPerm = new(targetIdentity, physicalObjectID, level);
                 ctx.Db.PhysicalObjectPermission.Insert(newPoPerm);
             }
+
+            ClientLog.Info(ctx, $"Set permission for {targetIdentity} on physical object {physicalObjectID} to {level}.");
         }
 
         [Reducer]
         public static void SetPhysicalObjectName(ReducerContext ctx, string PhysicalObjectID, string name)
         {
             string poPermID = PhysicalObjectPermission.GetIdentity(ctx.Sender, PhysicalObjectID);
-            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is PhysicalObjectPermission poPerm &&
-                poPerm.Level >= Permission.Level.Admin &&
-                ctx.Db.PhysicalObject.identity.Find(PhysicalObjectID) is PhysicalObject physicalObject)
+
+            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is not PhysicalObjectPermission poPerm ||
+                poPerm.Level < Permission.Level.Admin)
             {
-                physicalObject.Name = name;
-                ctx.Db.PhysicalObject.identity.Update(physicalObject);
+                ClientLog.Warning(ctx, "You do not have permission to set the name of this physical object.");
+                return;
             }
+
+            if (ctx.Db.PhysicalObject.identity.Find(PhysicalObjectID) is not PhysicalObject physicalObject)
+            {
+                ClientLog.Warning(ctx, "Physical object not found.");
+                return;
+            }
+
+            physicalObject.Name = name;
+            ctx.Db.PhysicalObject.identity.Update(physicalObject);
+
+            ClientLog.Info(ctx, $"Set name of physical object {PhysicalObjectID} to {name}.");
         }
 
         [Reducer]
-        public static void SetPhysicalObjectParentIdentity(ReducerContext ctx, string plotIdentity)
+        public static void SetPhysicalObjectParentIdentity(ReducerContext ctx, string physicalObjectID, string plotIdentity)
         {
-            string poPermID = PhysicalObjectPermission.GetIdentity(ctx.Sender, plotIdentity);
-            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is PhysicalObjectPermission poPerm &&
-                poPerm.Level >= Permission.Level.Admin &&
-                ctx.Db.PhysicalObject.identity.Find(plotIdentity) is PhysicalObject physicalObject)
+            string poPermID = PhysicalObjectPermission.GetIdentity(ctx.Sender, physicalObjectID);
+
+            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is not PhysicalObjectPermission poPerm ||
+                poPerm.Level < Permission.Level.Admin)
             {
-                physicalObject.ParentIdentity = plotIdentity;
-                ctx.Db.PhysicalObject.identity.Update(physicalObject);
+                ClientLog.Warning(ctx, "You do not have permission to set the name of this physical object.");
+                return;
             }
+
+            if (ctx.Db.PhysicalObject.identity.Find(physicalObjectID) is not PhysicalObject physicalObject)
+            {
+                ClientLog.Warning(ctx, "Physical object not found.");
+                return;
+            }
+
+            if (ctx.Db.Plot.identity.Find(plotIdentity) is not Plot plot)
+            {
+                ClientLog.Warning(ctx, "Plot not found.");
+                return;
+            }
+
+            physicalObject.ParentIdentity = plotIdentity;
+            ctx.Db.PhysicalObject.identity.Update(physicalObject);
+
+            ClientLog.Info(ctx, $"Set parent identity of physical object {physicalObjectID} to plot {plotIdentity}.");
         }
 
         [Reducer]
         public static void SetPhysicalObjectPosition(ReducerContext ctx, string physicalObjectID, int xPos, int yPos)
         {
             string poPermID = PhysicalObjectPermission.GetIdentity(ctx.Sender, physicalObjectID);
-            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is PhysicalObjectPermission poPerm &&
-                poPerm.Level >= Permission.Level.Admin &&
-                ctx.Db.PhysicalObject.identity.Find(physicalObjectID) is PhysicalObject physicalObject)
+
+            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is not PhysicalObjectPermission poPerm ||
+                poPerm.Level < Permission.Level.Admin)
             {
-                physicalObject.XPos = xPos;
-                physicalObject.YPos = yPos;
-                ctx.Db.PhysicalObject.identity.Update(physicalObject);
+                ClientLog.Warning(ctx, "You do not have permission to set the position of this physical object.");
+                return;
             }
+
+            if (ctx.Db.PhysicalObject.identity.Find(physicalObjectID) is not PhysicalObject physicalObject)
+            {
+                ClientLog.Warning(ctx, "Physical object not found.");
+                return;
+            }
+
+            // TODO: Check if position is valid (e.g. not colliding with other objects, in bounds of the plot, etc.)
+
+            physicalObject.XPos = xPos;
+            physicalObject.YPos = yPos;
+            ctx.Db.PhysicalObject.identity.Update(physicalObject);
+
+            ClientLog.Info(ctx, $"Set position of physical object {physicalObjectID} to ({xPos}, {yPos}).");
         }
 
         [Reducer]
         public static void DestroyPhysicalObject(ReducerContext ctx, string pysicalObjectID)
         {
             string poPermID = PhysicalObjectPermission.GetIdentity(ctx.Sender, pysicalObjectID);
-            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is PhysicalObjectPermission poPerm &&
-                poPerm.Level >= Permission.Level.Admin &&
-                ctx.Db.PhysicalObject.identity.Find(pysicalObjectID) is PhysicalObject physicalObject)
+
+            if (ctx.Db.PhysicalObjectPermission.identity.Find(poPermID) is not PhysicalObjectPermission poPerm ||
+                poPerm.Level < Permission.Level.Admin)
             {
-                foreach (PhysicalObjectPermission permission in ctx.Db.PhysicalObjectPermission.Iter()
-                         .Where(p => p.PhysicalObjectIdentity == physicalObject.identity))
-                {
-                    ctx.Db.PhysicalObjectPermission.Delete(permission);
-                }
-
-                foreach (Hardpoint hardpoint in ctx.Db.Hardpoint.Iter()
-                         .Where(h => h.PhysicalObjectIdentity == physicalObject.identity))
-                {
-                    HardpointReducers.DestroyHardpoint(ctx, hardpoint.identity);
-                }
-
-                ctx.Db.PhysicalObject.Delete(physicalObject);
+                ClientLog.Warning(ctx, "You do not have permission to destroy this physical object.");
+                return;
             }
+
+            if (ctx.Db.PhysicalObject.identity.Find(pysicalObjectID) is not PhysicalObject physicalObject)
+            {
+                ClientLog.Warning(ctx, "Physical object not found.");
+                return;
+            }
+
+            foreach (PhysicalObjectPermission permission in ctx.Db.PhysicalObjectPermission.Iter()
+                     .Where(p => p.PhysicalObjectIdentity == physicalObject.identity))
+            {
+                ctx.Db.PhysicalObjectPermission.Delete(permission);
+            }
+
+            foreach (Hardpoint hardpoint in ctx.Db.Hardpoint.Iter()
+                     .Where(h => h.PhysicalObjectIdentity == physicalObject.identity))
+            {
+                HardpointReducers.DestroyHardpoint(ctx, hardpoint.identity);
+            }
+
+            ctx.Db.PhysicalObject.Delete(physicalObject);
+
+            ClientLog.Info(ctx, $"Destroyed physical object {pysicalObjectID}.");
         }
     }
 
@@ -328,39 +390,55 @@ public static partial class Module
         public static void CreateHardpoint(ReducerContext ctx, string physicalObjectID, int size)
         {
             string permID = PhysicalObjectPermission.GetIdentity(ctx.Sender, physicalObjectID);
-            if (ctx.Db.PhysicalObjectPermission.identity.Find(permID) is PhysicalObjectPermission perm &&
-                perm.Level >= Permission.Level.Admin)
-            {
-                Hardpoint hardpoint = new()
-                {
-                    identity = Guid.NewGuid().ToString(),
-                    PhysicalObjectIdentity = physicalObjectID,
-                    Size = size,
-                };
 
-                ctx.Db.Hardpoint.Insert(hardpoint);
+            if (ctx.Db.PhysicalObjectPermission.identity.Find(permID) is not PhysicalObjectPermission poPerm ||
+                poPerm.Level < Permission.Level.Admin)
+            {
+                ClientLog.Warning(ctx, "You do not have permission to create a hardpoint on this physical object.");
+                return;
             }
+
+            Hardpoint hardpoint = new()
+            {
+                identity = Guid.NewGuid().ToString(),
+                PhysicalObjectIdentity = physicalObjectID,
+                Size = size,
+            };
+
+            ctx.Db.Hardpoint.Insert(hardpoint);
+
+            ClientLog.Info(ctx, $"Created hardpoint {hardpoint.identity} on physical object {physicalObjectID} with size {size}.");
         }
 
         [Reducer]
         public static void SetHardpointPermission(ReducerContext ctx, Identity targetIdentity, string hardpointID, int level)
         {
             if (ctx.Sender.Equals(targetIdentity))
+            {
+                ClientLog.Warning(ctx, "Cannot set permission for yourself.");
                 return;
+            }
 
             string actingPermID = HardpointPermission.GetIdentity(ctx.Sender, hardpointID);
             string targetPermID = HardpointPermission.GetIdentity(targetIdentity, hardpointID);
 
             if (ctx.Db.HardpointPermission.identity.Find(actingPermID) is not HardpointPermission actingPerm)
+            {
+                ClientLog.Warning(ctx, "You do not have permission to set permissions for this hardpoint.");
                 return;
+            }
 
             if (actingPerm.Level < Permission.Level.Admin)
+            {
+                ClientLog.Warning(ctx, "You do not have permission to set permissions for this hardpoint.");
                 return;
+            }
 
             if (ctx.Db.HardpointPermission.identity.Find(targetPermID) is HardpointPermission targetPerm)
             {
                 if (targetPerm.Level >= actingPerm.Level)
                 {
+                    ClientLog.Warning(ctx, "Cannot lower permission of a higher or equal level permission.");
                     return;
                 }
 
@@ -372,6 +450,7 @@ public static partial class Module
                 {
                     if (actingPerm.Level < level)
                     {
+                        ClientLog.Warning(ctx, "Cannot set permission to something higher than your own.");
                         return;
                     }
                     targetPerm.Level = level;
@@ -384,31 +463,48 @@ public static partial class Module
                     return;
 
                 if (actingPerm.Level < level)
+                {
+                    ClientLog.Warning(ctx, "Cannot set permission to something higher than your own.");
                     return;
+                }
 
                 HardpointPermission newPerm = new(targetIdentity, hardpointID, level);
                 ctx.Db.HardpointPermission.Insert(newPerm);
             }
+
+            ClientLog.Info(ctx, $"Set permission for {targetIdentity} on hardpoint {hardpointID} to {level}.");
         }
 
         [Reducer]
         public static void DestroyHardpoint(ReducerContext ctx, string hardpointID)
         {
             string permID = HardpointPermission.GetIdentity(ctx.Sender, hardpointID);
-            if (ctx.Db.HardpointPermission.identity.Find(permID) is HardpointPermission perm &&
-                perm.Level >= Permission.Level.Admin &&
-                ctx.Db.Hardpoint.identity.Find(hardpointID) is Hardpoint hardpoint)
+
+            if (ctx.Db.HardpointPermission.identity.Find(permID) is not HardpointPermission actingPerm ||
+                actingPerm.Level < Permission.Level.Admin)
             {
-                foreach (HardpointPermission permission in ctx.Db.HardpointPermission.Iter()
-                         .Where(p => p.HardpointIdentity == hardpoint.identity))
-                {
-                    ctx.Db.HardpointPermission.Delete(permission);
-                }
-
-                // TODO: Destroy all items in hardpoint
-
-                ctx.Db.Hardpoint.Delete(hardpoint);
+                ClientLog.Warning(ctx, "You do not have permission to destroy this hardpoint.");
+                return;
             }
+
+            if (ctx.Db.Hardpoint.identity.Find(hardpointID) is not Hardpoint hardpoint)
+            {
+                ClientLog.Warning(ctx, "Hardpoint not found.");
+                return;
+            }
+
+            foreach (HardpointPermission permission in ctx.Db.HardpointPermission.Iter()
+                     .Where(p => p.HardpointIdentity == hardpoint.identity))
+            {
+                ctx.Db.HardpointPermission.Delete(permission);
+            }
+
+            // TODO: Destroy all items in hardpoint
+            ClientLog.Info(ctx, "Destroying items in hardpoint is not yet implemented.");
+
+            ctx.Db.Hardpoint.Delete(hardpoint);
+
+            ClientLog.Info(ctx, $"Destroyed hardpoint {hardpointID}.");
         }
     }
 
