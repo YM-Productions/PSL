83dd5563b22cd06d48d3f0c550eecff08eb1519d
Mungg03 <lars.steinhardt2003@gmail.com>
2025-06-08 21:40:55 +0200

    convenience change Login on Enter

--- Client_PSL/Views/LoginRegister/LoginView.axaml.cs
+++ Client_PSL/Views/LoginRegister/LoginView.axaml.cs
@@ -1,5 +1,5 @@
-using System.Reflection;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Client_PSL.ViewModels;
 
@@ -7,12 +7,25 @@ namespace Client_PSL.Views;
 
 public partial class LoginView : UserControl
 {
+    
+    
+    
     public LoginView()
     {
         InitializeComponent();
     }
+    
+    private void OnEnter(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter &&
+            DataContext is LoginViewModel vm)
+        {
+            vm.LoginAttempt();
+            e.Handled = true;
+        }
+    }
 
-    private void OnLoginClick(object? sender, RoutedEventArgs e)
+    private void OnLoginClick(object? sender, RoutedEventArgs routedEventArgs)
     {
         if (sender is Button && DataContext is LoginViewModel vm)
         { 
